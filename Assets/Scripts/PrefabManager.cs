610e62b4d43c0ceff51d0a142029ed9ea6676f73
Looweez <59008362+Looweez@users.noreply.github.com>
2025-04-02 08:14:25 +0200

    k

--- Assets/Scripts/PrefabManager.cs
+++ Assets/Scripts/PrefabManager.cs
@@ -5,44 +5,63 @@ public class PrefabManager : MonoBehaviour
 {
     [SerializeField] private float xPlayerPosition = 0f;
     
-    [SerializeField] private float yPrefabPosition = 0f;
-    [SerializeField] private float zPrefabPosition = 0f;
-    [SerializeField] private float xPrefabPosition = 0f;
+    [SerializeField] private float prefabMoveSpeed = 5f;
+    [SerializeField] private float xPrefabPosition = 50f;
+    [SerializeField] private float zPrefabMin = -10f, zPrefabMax = 10f;
     
-    List<GameObject[,,] > Obstacles = new List<GameObject[,,]>(); //rows first, columns second
-        
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject[] obstacles;
+    //List<GameObject> Obstacles = new List<GameObject>(); //rows first, columns second
+    
     void Start()
     {
-        InvokeRepeating(nameof(SpawnPrefabs), 1.0f, 10f);  //spawns prefabs in intervals
+        InvokeRepeating(nameof(SpawnPrefabs), 1.0f, 2f);  //spawns prefabs in intervals
         InvokeRepeating(nameof(DeletePrefabs), 1.0f, 12f);  //deletes prefabs after a certain num of secs
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        transform.Translate(200f * Time.deltaTime, 0, 0);  //constant mvoing
+        MovePrefabs();
+    }
+    
+    void MovePrefabs()
+    {
+        //transform.Translate(200f * Time.deltaTime, 0, 0);  //constant mvoing
+        
+        for (int i = 0; i < obstacles.Length; i++)
+        {
+            if (obstacles[i] != null)
+            {
+                obstacles[i].transform.Translate(-prefabMoveSpeed, 0, 0 * Time.deltaTime);
+            }
+        }
     }
 
     private void SpawnPrefabs()
     {
         //randomly spawns a plain obstacle
         //has a chance of spawning an obstacle on top of the basic plat form
-        for (int i = 0; i < Obstacles.Count; i++)  //row
+        /*for (int i = 0; i < Obstacles.Count; i++)  //row
         {
             for (int j = 0; j < Obstacles[i].Length; j++) //column
             {
-                Resources.Load<GameObject>("Prefabs/Level1/BasicPlat");
+                GameObject jumpBar = Instantiate(Resources.Load<GameObject>("Prefabs/Level1/BasicPlat"), new Vector3(xPrefabPosition, yPrefabPosition, zPrefabPosition), Quaternion.identity); //idk if this is right but it autofilled so
                 j++;
             }
-
             i++;
-        }
-        Resources.Load<GameObject>("Prefabs/Level1/BasicPlat");
+        }*/
+
+        if (obstacles.Length == 0) return;
+        
+        GameObject spawnPrefab = obstacles[Random.Range(0, obstacles.Length)]; //spawn random prefab from array
+        
+        float randomZ = Random.Range(zPrefabMin, zPrefabMax); //to spawn obstacle randomly in front of player
+        GameObject newPrefab = Instantiate(spawnPrefab, new Vector3(xPrefabPosition, 0.0f, randomZ), Quaternion.identity); //autofilled LOL i hope its right
+        
     }
 
     private void DeletePrefabs()
     {
+        //IncrementScore();
         //deletes prefabs after they pass the player
         Resources.UnloadUnusedAssets();
     }
